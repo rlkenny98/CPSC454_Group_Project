83e422e61d6005a24b55aa5e0496d927e5b3d139
rosemary9820 <rosemary98@csu.fullerton.edu>
2021-10-08 16:55:32 -0700

    upload file
    added a simple interface to upload files to django

--- django_project/webpage/views.py
+++ django_project/webpage/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 # TO-DO @Maria: Link to any html files here
 #EXAMPlE
 
   
 def home(request):
+    if request.method == "POST":
+        # if the post request has a file under the input name 'document', then save the file.
+        request_file = request.FILES['document'] if 'document' in request.FILES else None
+        if request_file:
+                # save attatched file
+
+                # create a new instance of FileSystemStorage
+                fs = FileSystemStorage()
+                file = fs.save(request_file.name, request_file)
+                # the fileurl variable now contains the url to the file. This can be used to serve the file when needed.
+                fileurl = fs.url(file)
+
     return render(request, 'webpage/home.html')
-
-
 def about(request):
     return render(request, 'webpage/about.html')
