7083427563a35c7ecc975eeb7e36d03e56488cdc
Robert Kenny <60153147+rlkenny98@users.noreply.github.com>
2021-11-30 11:49:29 -0800

    Added Backend Logic for Downloads
    Implemented the user file sort logic that only displays files belonging to the current user. Changed frontend to accept a dictionary object to accommodate this change. Also changed '+' in the file name to '_' because it was causing weird error when clicking on the file link.

--- django_project/webpage/views.py
+++ django_project/webpage/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django_project.custom_storage import MediaStorage
-from django.core.files.storage import default_storage
+#from django.core.files.storage import default_storage
 from users.models import Profile
 from django.contrib.auth.models import User
-
+from storages.backends.s3boto3 import S3Boto3Storage
 import hashlib
 # TO-DO @Maria: Link to any html files here
 #EXAMPlE
+
+token = ''
+token_size = 0
 
 @login_required
 def home(request):
@@ -26,7 +29,7 @@ def upload(request):
                 # save attatched file
                 # create a new instance of FileSystemStorage
                 media = MediaStorage()
-                filename = f'{token.hexdigest()}+{request_file.name}'
+                filename = f'{token.hexdigest()}_{request_file.name}'
                 print(filename)
                 file = media.save(filename, request_file)
                 # file = media.upload_file(request_file.name, request_file, ExtraArgs={"Metadata": {"x-amz-meta-filetoken": token}})
@@ -46,9 +49,26 @@ def uploadDownload(request):
 @login_required
 def download(request):
     media = MediaStorage()
+    token = hashlib.md5(str.encode(request.user.username))
+    token_size = token.__sizeof__()
+    token_str = token.hexdigest()
+    dict = {}
 
     filelist = media.listdir("")
+    filelist = filelist[1]
+    #print(filelist[1])
+    #Size of token value we have to compare to
+    print(token_size)
+    print(f'Files for {request.user.username}:')
+    for file in filelist:
+        #Compare the first n characters of file name with the hashed token string value
+        #print(file[:token_size])
+        #print(token_str)
+        if file[:token_size] == token_str:
+            # Removing token value and '+' character from name
+            cleaned_name = file[token_size+1:]
+            #print(cleaned_name)
+            dict[cleaned_name] = file
+            #dictList.append(dict)
 
-    filelist = filelist[1] 
-     
-    return render(request, 'webpage/download.html',{'filelist':filelist})
+    return render(request, 'webpage/download.html',{'dictList':dict})
